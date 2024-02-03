3e1d8863c88f16d4f2128efea8c99618c5e7369d
emredurak01 <emredurak01@gmail.com>
2024-02-03 19:33:57 +0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,195 @@
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import {
+  Cog6ToothIcon,
+  XMarkIcon,
+  CircleStackIcon,
+  ChartPieIcon,
+} from "@heroicons/react/24/outline";
+import { Bars3Icon, MagnifyingGlassIcon } from "@heroicons/react/20/solid";
+import Overview from "./Overview";
+
+const navigation = [
+  { name: "Database", href: "#", icon: CircleStackIcon, current: true },
+  { name: "Charts", href: "#", icon: ChartPieIcon, current: false },
+  { name: "Settings", href: "#", icon: Cog6ToothIcon, current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <>
+      <div>
+        <Transition.Root show={sidebarOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-50 xl:hidden"
+            onClose={setSidebarOpen}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-900/80" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                      <button
+                        type="button"
+                        className="-m-2.5 p-2.5"
+                        onClick={() => setSidebarOpen(false)}
+                      >
+                        <span className="sr-only">Close sidebar</span>
+                        <XMarkIcon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  {/* Sidebar component */}
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 ring-1 ring-white/10">
+                    <div className="flex h-16 shrink-0 items-center">
+                      <img className="h-8 w-auto" src="" alt="Logo" />
+                    </div>
+                    <nav className="flex flex-1 flex-col">
+                      <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                        <li>
+                          <ul role="list" className="-mx-2 space-y-1">
+                            {navigation.map((item) => (
+                              <li key={item.name}>
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    item.current
+                                      ? "bg-gray-800 text-white"
+                                      : "text-gray-400 hover:text-white hover:bg-gray-800",
+                                    "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                                  )}
+                                >
+                                  <item.icon
+                                    className="h-6 w-6 shrink-0"
+                                    aria-hidden="true"
+                                  />
+                                  {item.name}
+                                </a>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden xl:fixed xl:inset-y-0 xl:z-50 xl:flex xl:w-72 xl:flex-col">
+          {/* Sidebar component*/}
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-black/10 px-6 ring-1 ring-white/5">
+            <div className="flex h-16 shrink-0 items-center">
+              <img className="h-8 w-auto" src="" alt="Logo" />
+            </div>
+            <nav className="flex flex-1 flex-col">
+              <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                <li>
+                  <ul role="list" className="-mx-2 space-y-1">
+                    {navigation.map((item) => (
+                      <li key={item.name}>
+                        <a
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-800 text-white"
+                              : "text-gray-400 hover:text-white hover:bg-gray-800",
+                            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+                          )}
+                        >
+                          <item.icon
+                            className="h-6 w-6 shrink-0"
+                            aria-hidden="true"
+                          />
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div className="xl:pl-72">
+          {/* Sticky search header */}
+          <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-6 border-b border-white/5 bg-gray-900 px-4 shadow-sm sm:px-6 lg:px-8">
+            <button
+              type="button"
+              className="-m-2.5 p-2.5 text-white xl:hidden"
+              onClick={() => setSidebarOpen(true)}
+            >
+              <span className="sr-only">Open sidebar</span>
+              <Bars3Icon className="h-5 w-5" aria-hidden="true" />
+            </button>
+
+            <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+              <form className="flex flex-1" action="#" method="GET">
+                <label htmlFor="search-field" className="sr-only">
+                  Search
+                </label>
+                <div className="relative w-full">
+                  <MagnifyingGlassIcon
+                    className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-500"
+                    aria-hidden="true"
+                  />
+                  <input
+                    id="search-field"
+                    className="block h-full w-full border-0 bg-transparent py-0 pl-8 pr-0 text-white focus:ring-0 sm:text-sm"
+                    placeholder="Search..."
+                    type="search"
+                    name="search"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+
+          <Overview />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/Overview.jsx
@@ -0,0 +1,158 @@
+const stats = [
+  { name: "Number of users", value: "405" },
+  { name: "Connect time", value: "3.65", unit: "seconds" },
+  { name: "Number of servers", value: "1" },
+  { name: "Success rate", value: "98.5%" },
+];
+const statuses = {
+  Completed: "text-green-400 bg-green-400/10",
+  Error: "text-rose-400 bg-rose-400/10",
+};
+const items = [
+  {
+    id: 1,
+    name: "Emu Bird",
+    email: "emu@bird.com",
+    dob: "10-12-1932",
+    phone: "555-555-55-55",
+    adress: "Australia",
+  },
+
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Overview() {
+  return (
+    <>
+      <main>
+        <header>
+          {/* Heading */}
+          <div className="flex flex-col items-start justify-between gap-x-8 gap-y-4 bg-gray-700/10 px-4 py-4 sm:flex-row sm:items-center sm:px-6 lg:px-8">
+            <div>
+              <div className="flex items-center gap-x-3">
+                <div className="flex-none rounded-full bg-green-400/10 p-1 text-green-400">
+                  <div className="h-2 w-2 rounded-full bg-current" />
+                </div>
+                <h1 className="flex gap-x-3 text-base leading-7">
+                  <span className="font-semibold text-white">
+                    Server status
+                  </span>
+                  <span className="text-gray-600">/</span>
+                  <span className="font-semibold text-white">Spring</span>
+                </h1>
+              </div>
+              <p className="mt-2 text-xs leading-6 text-gray-400">Connected</p>
+            </div>
+          </div>
+
+          {/* Stats */}
+          <div className="grid grid-cols-1 bg-gray-700/10 sm:grid-cols-2 lg:grid-cols-4">
+            {stats.map((stat, statIdx) => (
+              <div
+                key={stat.name}
+                className={classNames(
+                  statIdx % 2 === 1
+                    ? "sm:border-l"
+                    : statIdx === 2
+                    ? "lg:border-l"
+                    : "",
+                  "border-t border-white/5 py-6 px-4 sm:px-6 lg:px-8"
+                )}
+              >
+                <p className="text-sm font-medium leading-6 text-gray-400">
+                  {stat.name}
+                </p>
+                <p className="mt-2 flex items-baseline gap-x-2">
+                  <span className="text-4xl font-semibold tracking-tight text-white">
+                    {stat.value}
+                  </span>
+                  {stat.unit ? (
+                    <span className="text-sm text-gray-400">{stat.unit}</span>
+                  ) : null}
+                </p>
+              </div>
+            ))}
+          </div>
+        </header>
+
+        {/* List */}
+        <div className="border-t border-white/10 pt-11">
+          <h2 className="px-4 text-base font-semibold leading-7 text-white sm:px-6 lg:px-8">
+            Latest activity
+          </h2>
+          <table className="mt-6 w-full whitespace-nowrap text-left">
+            <colgroup>
+              <col className="w-full sm:w-2/12" />
+              <col className="lg:w-1/12" />
+              <col className="lg:w-1/12" />
+              <col className="lg:w-1/12" />
+              <col className="lg:w-1/12" />
+            </colgroup>
+            <thead className="border-b border-white/10 text-sm leading-6 text-white">
+              <tr>
+                <th
+                  scope="col"
+                  className="py-2 pl-4 pr-8 font-semibold sm:pl-6 lg:pl-8"
+                >
+                  User
+                </th>
+                <th
+                  scope="col"
+                  className="hidden py-2 pl-0 pr-8 font-semibold md:table-cell lg:pr-20"
+                >
+                  Email
+                </th>
+                <th
+                  scope="col"
+                  className="hidden py-2 pl-0 pr-8 font-semibold md:table-cell lg:pr-20"
+                >
+                  Date of birth
+                </th>
+                <th
+                  scope="col"
+                  className="hidden py-2 pl-0 pr-8 font-semibold md:table-cell lg:pr-20"
+                >
+                  Phone number
+                </th>
+                <th
+                  scope="col"
+                  className="hidden py-2 pl-0 pr-8 font-semibold md:table-cell lg:pr-20"
+                >
+                  Address
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-white/5">
+              {items.map((item) => (
+                <tr key={item.id}>
+                  <td className="py-4 pl-4 pr-8 sm:pl-6 lg:pl-8">
+                    <div className="flex items-center gap-x-4">
+                      <div className="truncate text-sm font-medium leading-6 text-white">
+                        {item.name}
+                      </div>
+                    </div>
+                  </td>
+                  <td className="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20">
+                    {item.email}
+                  </td>
+                  <td className="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20">
+                    {item.dob}
+                  </td>
+                  <td className="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20">
+                    {item.phone}
+                  </td>
+                  <td className="hidden py-4 pl-0 pr-8 text-sm leading-6 text-gray-400 md:table-cell lg:pr-20">
+                    {item.adress}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </>
+  );
+}
